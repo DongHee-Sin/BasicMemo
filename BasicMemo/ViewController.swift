8184a255f08195a999f60ef66467c6fa14d6a56c
DongHee-Sin <voslr0601@naver.com>
2022-08-31 14:24:49 +0900

    Initial Commit

---
+++ BasicMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicMemo
+//
+//  Created by 신동희 on 2022/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
