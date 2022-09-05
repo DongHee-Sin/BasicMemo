fb4536596c7347427333bec42dd356bfb983a35d
DongHee-Sin <voslr0601@naver.com>
2022-09-05 15:46:59 +0900

    DateFormatter) 이번주 작성 메모 계산하는 로직 수정 수정 전 : 현재 날짜에서 -7 수정 후 : 달력 기준으로 같은 주차에 있는지 확인

--- BasicMemo/Manager/DateFormatManager.swift
+++ BasicMemo/Manager/DateFormatManager.swift
@@ -15,13 +15,17 @@ final class DateFormatManager {
     private init() {}
     
     
-    private let calendar = Calendar.current
+    private let calendar: Calendar = {
+        var calendar = Calendar.current
+        calendar.firstWeekday = 2
+        calendar.locale = Locale.current
+        return calendar
+    }()
     
     
     private let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.timeZone = TimeZone(identifier: "Asia/Seoul")
-        
         return formatter
     }()
     
@@ -31,12 +35,22 @@ final class DateFormatManager {
         
         if dateDifference < 1 {
             dateFormatter.dateFormat = "a hh:mm"
-        }else if dateDifference < 7 {
+        }else if calculateWeekNumberDifference(date: date) < 1 {
             dateFormatter.dateFormat = "E"
         }else {
             dateFormatter.dateFormat = "yyyy.MM.dd a hh:mm"
         }
         
         return dateFormatter.string(from: date) + "  "
+    }
+    
+    
+    private func calculateWeekNumberDifference(date: Date) -> Int {
+        let weekNumberForStartDate = calendar.component(.weekOfYear, from: date)
+        let weekNumberForEndDate = calendar.component(.weekOfYear, from: Date())
+
+       let difference = weekNumberForEndDate - weekNumberForStartDate
+
+       return difference
     }
 }
